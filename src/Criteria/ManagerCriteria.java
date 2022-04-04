cb8e86d6cb5f914ee848a7e304c8bc6164bee572
MuhammadTsabitAlAzam <azamm176.ma@gmail.com>
2022-04-04 09:49:45 +0700

    Quis Prakt PBO

---
+++ src/Criteria/ManagerCriteria.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Criteria;
+import Pendaftar.Pendaftar;
+/**
+ *
+ * @author ASUS
+ */
+public class ManagerCriteria extends Pendaftar implements Perhitungan {
+     public ManagerCriteria(String nama, int nilai1, int nilai2, int nilai3) {
+        super(nama, nilai1, nilai2, nilai3);
+    }
+
+    public void setNilai1(int nilai1) {
+        this.nilai1 = nilai1;
+    }
+
+    public void setNilai2(int nilai2) {
+        this.nilai2 = nilai2;
+    }
+
+    public void setNilai3(int nilai3) {
+        this.nilai3 = nilai3;
+    }
+
+    @Override
+    public double nilai() {
+        return (super.nilai1 * 0.4) + (super.nilai2 * 0.3) + (super.nilai3 * 0.3);
+    }
+
+    @Override
+    public String keterangan() {
+        if(this.nilai() >= 85) return "LULUS\nSelamat kepada " + this.nama + " telah diterima sebagai Manager";
+        else return "GAGAL\nMohon maaf kepada " + this.nama + " telah ditolak sebagai Manager";
+    }
+}
---
+++ src/Criteria/Perhitungan.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Criteria;
+
+/**
+ *
+ * @author ASUS
+ */
+public interface Perhitungan {
+    public double nilai();
+    public String keterangan();
+}
